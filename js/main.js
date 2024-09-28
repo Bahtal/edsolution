b0ff59932245491c697f2fd8c70e97947210e993
Bahtal <bahtal777artem@gmail.com>
2024-09-28 16:25:19 +0300

    with adaptacy

--- js/main.js
+++ js/main.js
@@ -74,23 +74,19 @@ document.addEventListener('DOMContentLoaded', function () {
 			const swiper = new Swiper('.swiper-container', {
 				loop: true,
 				slidesPerView: 1,
-				spaceBetween: 10,
+				spaceBetween: 30,
 				pagination: {
 						el: '.swiper-pagination',
 						clickable: true,
 			},
-			navigation: {
-					nextEl: '.swiper-button-next',
-					prevEl: '.swiper-button-prev',
-			},
 			breakpoints: {
 					368: {
 							slidesPerView: 1,
-							spaceBetween: 10,
+							spaceBetween: 30,
 					},
 					769: {
 							slidesPerView: 2,
-							spaceBetween: 20,
+							spaceBetween: 40,
 					}
 	},
 });
